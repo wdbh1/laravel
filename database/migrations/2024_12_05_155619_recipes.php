6e9585187faedfa3a3825aa377013f7dbe3c5e52
unknown <[email protected]>
2024-12-05 21:07:17 +0500

    Migrations task

---
+++ database/migrations/2024_12_05_155619_recipes.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+  Schema::create('recipes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('dish_id');
+            $table->unsignedBigInteger('ingredient_id');
+            $table->decimal('quantity', 10, 2); // Количество с плавающей точкой
+            $table->timestamps();
+            $table->foreign('dish_id')->references('id')->on('dishes')->onDelete('cascade');
+            $table->foreign('ingredient_id')->references('id')->on('ingredients')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
